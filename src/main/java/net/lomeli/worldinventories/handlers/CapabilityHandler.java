60a83efda907453c8ed3f79585899fc71116ecdc
Lomeli <anthonylomeli11@gmail.com>
2019-12-06 00:44:28 -0800

    We should really copy data every time the Clone event triggers.
    Simply rearranged the logic so we copy everything every Clone event except when the player dies.

--- src/main/java/net/lomeli/worldinventories/handlers/CapabilityHandler.java
+++ src/main/java/net/lomeli/worldinventories/handlers/CapabilityHandler.java
@@ -23,16 +23,15 @@ public class CapabilityHandler {
 
     @SubscribeEvent
     public static void playerClone(PlayerEvent.Clone event) {
-        if (event.isWasDeath()) {
-            IPlayerDimInv oldPlayerInfo = PlayerDimInv.getDimInventories(event.getOriginal());
-            IPlayerDimInv newPlayerInfo = PlayerDimInv.getDimInventories(event.getPlayer());
-            if (oldPlayerInfo != null && newPlayerInfo != null) {
+        IPlayerDimInv oldPlayerInfo = PlayerDimInv.getDimInventories(event.getOriginal());
+        IPlayerDimInv newPlayerInfo = PlayerDimInv.getDimInventories(event.getPlayer());
+        if (oldPlayerInfo != null && newPlayerInfo != null) {
+            if (event.isWasDeath()) {
                 // Remove the inventory from the dimension the player died in.
                 ResourceLocation dimID = event.getOriginal().dimension.getRegistryName();
                 oldPlayerInfo.getDimInventories(dimID).clear();
-                
-                newPlayerInfo.copy(oldPlayerInfo.getInventories());
             }
+            newPlayerInfo.copy(oldPlayerInfo.getInventories());
         }
     }
 }
