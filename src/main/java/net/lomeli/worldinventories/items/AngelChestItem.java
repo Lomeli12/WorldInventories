1ea79aa94a7dfaceb6e1622947cfdb2a657c8608
Lomeli <anthonylomeli11@gmail.com>
2019-12-07 15:24:23 -0800

    Localization and registering Angel Chest.

---
+++ src/main/java/net/lomeli/worldinventories/items/AngelChestItem.java
@@ -0,0 +1,37 @@
+package net.lomeli.worldinventories.items;
+
+import net.lomeli.worldinventories.WorldInventories;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class AngelChestItem extends Item {
+    public AngelChestItem() {
+        super(new Item.Properties().rarity(Rarity.EPIC).group(ItemGroup.MISC));
+        this.setRegistryName(WorldInventories.MOD_ID, "angel_chest");
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        tooltip.add(new TranslationTextComponent("item.worldinventories.angel_chest.info"));
+    }
+
+    public static int getAngelChestSlot(PlayerEntity player) {
+        for (int slot = 0; slot < player.inventory.getSizeInventory(); slot++) {
+            ItemStack stack = player.inventory.getStackInSlot(slot);
+            if (stack.getItem() == ModItems.angelChest)
+                return slot;
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/net/lomeli/worldinventories/items/ModItems.java
@@ -0,0 +1,20 @@
+package net.lomeli.worldinventories.items;
+
+import net.lomeli.worldinventories.WorldInventories;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
+
+@Mod.EventBusSubscriber(modid = WorldInventories.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModItems {
+
+    @ObjectHolder(WorldInventories.MOD_ID + ":angel_chest")
+    public static Item angelChest;
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(angelChest = new AngelChestItem());
+    }
+}
