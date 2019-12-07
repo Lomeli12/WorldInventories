3b4a5982524160f1f5055026f05a943312c683f6
Lomeli <anthonylomeli11@gmail.com>
2019-12-06 16:11:39 -0800

    Now handling player respawning and keepInventory gamerule. Fixes #2

--- src/main/java/net/lomeli/worldinventories/handlers/CapabilityHandler.java
+++ src/main/java/net/lomeli/worldinventories/handlers/CapabilityHandler.java
@@ -6,7 +6,6 @@ import net.lomeli.worldinventories.capabilities.PlayerDimInv;
 import net.lomeli.worldinventories.capabilities.PlayerDimInvProvider;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -25,13 +24,7 @@ public class CapabilityHandler {
     public static void playerClone(PlayerEvent.Clone event) {
         IPlayerDimInv oldPlayerInfo = PlayerDimInv.getDimInventories(event.getOriginal());
         IPlayerDimInv newPlayerInfo = PlayerDimInv.getDimInventories(event.getPlayer());
-        if (oldPlayerInfo != null && newPlayerInfo != null) {
-            if (event.isWasDeath()) {
-                // Remove the inventory from the dimension the player died in.
-                ResourceLocation dimID = event.getOriginal().dimension.getRegistryName();
-                oldPlayerInfo.getDimInventories(dimID).clear();
-            }
+        if (oldPlayerInfo != null && newPlayerInfo != null)
             newPlayerInfo.copy(oldPlayerInfo);
-        }
     }
 }
--- src/main/java/net/lomeli/worldinventories/handlers/InventoryHandler.java
+++ src/main/java/net/lomeli/worldinventories/handlers/InventoryHandler.java
@@ -2,14 +2,16 @@ package net.lomeli.worldinventories.handlers;
 
 import net.lomeli.worldinventories.WorldInventories;
 import net.lomeli.worldinventories.api.IDimensionInventory;
-import net.lomeli.worldinventories.api.SwapInventoryEvent;
 import net.lomeli.worldinventories.api.IPlayerDimInv;
+import net.lomeli.worldinventories.api.SwapInventoryEvent;
 import net.lomeli.worldinventories.capabilities.PlayerDimInv;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.GameRules;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -39,9 +41,38 @@ public class InventoryHandler {
     public static void changeDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
         PlayerEntity player = event.getPlayer();
         IPlayerDimInv dimInv = PlayerDimInv.getDimInventories(player);
-        IDimensionInventory prevDimInventory = dimInv.getDimInventories(event.getFrom().getRegistryName());
-        IDimensionInventory newDimInventory = dimInv.getDimInventories(event.getTo().getRegistryName());
+        fireSwapEvent(event.getPlayer(), dimInv, event.getFrom().getRegistryName(),
+                event.getTo().getRegistryName(), false, false);
+    }
+
+    @SubscribeEvent
+    public static void playerRespawn(PlayerEvent.PlayerRespawnEvent event) {
+        IPlayerDimInv dimInv = PlayerDimInv.getDimInventories(event.getPlayer());
+        boolean keepInventory = event.getPlayer().world.getGameRules().getBoolean(GameRules.KEEP_INVENTORY);
+        fireSwapEvent(event.getPlayer(), dimInv, dimInv.lastDimensionDiedIn(),
+                event.getPlayer().dimension.getRegistryName(), true, keepInventory);
+    }
+
+    private static void fireSwapEvent(PlayerEntity player, IPlayerDimInv dimInv, ResourceLocation prevDim,
+                                      ResourceLocation nextDim, boolean death, boolean keepInventory) {
+        if (dimInv == null || prevDim == null || nextDim == null)
+            return;
+        IDimensionInventory prevDimInventory = dimInv.getDimInventories(prevDim);
+        IDimensionInventory newDimInventory = dimInv.getDimInventories(nextDim);
         MinecraftForge.EVENT_BUS.post(new SwapInventoryEvent(player, prevDimInventory, newDimInventory));
+        if (death && !keepInventory)
+            prevDimInventory.clear(); //If keepInventory is off, clear prevDimInventory cause they dropped those items
+
         dimInv.addInventory(prevDimInventory);
+    }
+
+    @SubscribeEvent
+    public static void playerDeath(LivingDeathEvent event) {
+        if (event.getEntityLiving() instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+            IPlayerDimInv dimInv = PlayerDimInv.getDimInventories(player);
+            if (dimInv != null)
+                dimInv.setDimensionDiedIn(player.dimension.getRegistryName());
+        }
     }
 }
